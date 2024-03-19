8d2fcfb69e65221bacdfe9fd06b26ef3d8b3d35f
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2024-03-19 00:53:38 +0000

    Finish second tp4 exercise: Add UnitCubeQuad and apply texture to it

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -7,9 +7,15 @@ import { MyQuad } from "./MyQuad.js";
  * @param scene - Reference to MyScene object
  */
 export class MyUnitCubeQuad extends CGFobject {
-  constructor(scene) {
+  constructor(scene, top, bottom, front, back, left, right) {
     super(scene);
     this.initBuffers();
+    this.top = top;
+    this.bottom = bottom;
+    this.front = front;
+    this.back = back;
+    this.left = left;
+    this.right = right;
   }
 
   initBuffers() {
@@ -17,38 +23,80 @@ export class MyUnitCubeQuad extends CGFobject {
   }
 
   display() {
+    // Top
     this.scene.pushMatrix();
-    this.scene.translate(0, 0, 0.5);
+    this.scene.translate(0, 0.5, 0);
+    this.scene.rotate((3 * Math.PI) / 2, 1, 0, 0);
+    this.top.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
     this.quad.display();
     this.scene.popMatrix();
 
+    // Front
+    this.scene.pushMatrix();
+    this.scene.translate(0.5, 0, 0);
+    this.scene.rotate(Math.PI / 2, 0, 1, 0);
+    this.front.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
+    this.quad.display();
+    this.scene.popMatrix();
+
+    // Right
     this.scene.pushMatrix();
     this.scene.translate(0, 0, -0.5);
     this.scene.rotate(Math.PI, 0, 1, 0);
+    this.right.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
     this.quad.display();
     this.scene.popMatrix();
 
+    // Back
     this.scene.pushMatrix();
-    this.scene.translate(0, 0.5, 0);
-    this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+    this.scene.translate(-0.5, 0, 0);
+    this.scene.rotate((3 * Math.PI) / 2, 0, 1, 0);
+    this.back.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
     this.quad.display();
     this.scene.popMatrix();
 
+    // Left
+    this.scene.pushMatrix();
+    this.scene.translate(0, 0, 0.5);
+    this.left.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
+    this.quad.display();
+    this.scene.popMatrix();
+
+    // Bottom
     this.scene.pushMatrix();
     this.scene.translate(0, -0.5, 0);
     this.scene.rotate(Math.PI / 2, 1, 0, 0);
-    this.quad.display();
-    this.scene.popMatrix();
-
-    this.scene.pushMatrix();
-    this.scene.translate(0.5, 0, 0);
-    this.scene.rotate(-Math.PI / 2, 0, 1, 0);
-    this.quad.display();
-    this.scene.popMatrix();
-
-    this.scene.pushMatrix();
-    this.scene.translate(-0.5, 0, 0);
-    this.scene.rotate(Math.PI / 2, 0, 1, 0);
+    this.bottom.bind();
+    this.scene.gl.texParameteri(
+      this.scene.gl.TEXTURE_2D,
+      this.scene.gl.TEXTURE_MAG_FILTER,
+      this.scene.gl.NEAREST
+    );
     this.quad.display();
     this.scene.popMatrix();
   }
